12f7740ccc7aa4da1b08fcb98bb97b4fda3e0841
Jose Manuel <gigistudio.contact@gmail.com>
2024-03-21 12:11:29 +0100

    Cinta configurada v1 (a la espera de bugs)

--- Assets/AnimateConveyor.cs
+++ Assets/AnimateConveyor.cs
@@ -1,26 +1,48 @@
+using game4automation;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AnimateConveyor : MonoBehaviour
 {
+    public Drive ConveyorDrive;
+    public OPCUA_Interface PLC_Interface;
+
+    public string NodeIdPosition;
+    public string NodeIdVelocity;
+
+    public double initialPosition;
+    public double actualPosition;
+    public double actualVelocity;
+    private void Start()
+    {
+        initialPosition = (double)PLC_Interface.ReadNodeValue(NodeIdPosition);
+    }
+
     public Transform conveyor;
     public Transform alturaMinimaReferencia;
     public Transform alturaMaximaReferencia;
 
-    // Función para animar la plataforma
     public void Animar(float t)
     {
-        // Asegurarse de que t esté en el rango [0, 1]
         t = Mathf.Clamp01(t);
 
-        // Calcular la posición intermedia utilizando Lerp
         float alturaIntermedia = Mathf.Lerp(alturaMinimaReferencia.position.y, alturaMaximaReferencia.position.y, t);
 
-        // Actualizar la posición de la plataforma
         Vector3 nuevaPosicion = conveyor.position;
         nuevaPosicion.y = alturaIntermedia;
         conveyor.position = nuevaPosicion;
     }
+
+    void Update()
+    {
+        actualPosition = (double)PLC_Interface.ReadNodeValue(NodeIdPosition);
+        actualVelocity = (double)PLC_Interface.ReadNodeValue(NodeIdVelocity);
+
+        ConveyorDrive.CurrentPosition = (float)(actualPosition - initialPosition);
+        ConveyorDrive.CurrentSpeed = (float)actualVelocity;
+    }
+
+
 }
 
